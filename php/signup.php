7d4f5677467a2691992fd8dbe9cc1e63c9f30ac2
IotMilktea <pp60412@yahoo.com.tw>
2019-07-09 21:20:20 +0800

    update

--- php/signup.php
+++ php/signup.php
@@ -4,10 +4,10 @@ if(!isset($_POST['submit'])){
 exit("錯誤執行");
 }//判斷是否有submit操作
 
-$server="localhost";//主機
-$db_username="test123";//你的資料庫使用者名稱
-$db_password="test123";//你的資料庫密碼
-$dbname = "g1906";
+$server="140.130.19.38:58136";//主機
+$db_username="g1906";//你的資料庫使用者名稱
+$db_password="20191nchu_g06";//你的資料庫密碼
+$dbname = "g1906_test01";
 $con = new mysqli($server,$db_username,$db_password,$dbname);//連結資料庫
 if(!$con){
 die("can't connect".mysqli_error());//如果連結失敗輸出錯誤
