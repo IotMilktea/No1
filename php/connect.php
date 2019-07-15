039d3c32e0267f16ba8732a63237e9a5c2cffb23
IotMilktea <pp60412@yahoo.com.tw>
2019-07-15 22:05:03 +0800

    update

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
 $server="localhost";//主機
-$db_username="test123";//你的資料庫使用者名稱
-$db_password="test123";//你的資料庫密碼
-$dbname = "g1906";
+$db_username="g1906";//你的資料庫使用者名稱
+$db_password="20191nchu_g06";//你的資料庫密碼
+$dbname = "g1906_test01";
 $con = new mysqli($server,$db_username,$db_password,$dbname);//連結資料庫
 if(!$con){
 die("can't connect".mysqli_error());//如果連結失敗輸出錯誤
--- php/showData.php
+++ php/showData.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $server="localhost";//主機
 $db_username="g1906";//你的資料庫使用者名稱
 $db_password="20191nchu_g06";//你的資料庫密碼
@@ -7,7 +8,7 @@ $con = new mysqli($server,$db_username,$db_password,$dbname);//連結資料庫
 if(!$con){
 die("can't connect".mysqli_error());//如果連結失敗輸出錯誤
 }
-$email=$_POST['email'];
+$email=$_SESSION['email'];
 $q="SELECT * FROM `userdata` WHERE email='$email'";//從contact資料庫中選擇所有的資料表
 $reslut=mysqli_query($con,$q);//執行sql
 ?>
@@ -39,6 +40,8 @@ $rs=mysqli_fetch_row($reslut);
 
 <?php
 }
+
+
 if (!$reslut){
 die('Error: ' . mysqli_error($con));//如果sql執行失敗輸出錯誤
 }else{
@@ -47,4 +50,7 @@ die('Error: ' . mysqli_error($con));//如果sql執行失敗輸出錯誤
 mysqli_close($con);//關閉資料庫
 ?>
 </table>
+<body>
+<a href="http://140.130.19.38:58136/~g1906/test/php/welcome.php">回首頁</a>
+</body>
 </html>
